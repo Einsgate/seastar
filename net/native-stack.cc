343d84c09ada28761161bb6b91a54127c6def78e
duanjp8617 <duanjp8617@gmail.com>
2017-08-31 17:23:02 +0800

    add trace print

--- net/native-stack.cc
+++ net/native-stack.cc
@@ -152,7 +152,7 @@ public:
     virtual udp_channel make_udp_channel(ipv4_addr addr) override;
     virtual future<> initialize() override;
     static future<std::unique_ptr<network_stack>> create(boost::program_options::variables_map opts) {
-        printf("Thread %d: Start creating the native network stack\n");
+        printf("Thread %d: Start creating the native network stack\n", engine().cpu_id());
         if (engine().cpu_id() == 0) {
             create_native_net_device(opts);
         }
